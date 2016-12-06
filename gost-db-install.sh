aee26fc6700ac27fffac9e15d513d44c06a77e0d
Bert Temme <btemme@hotmail.com>
2016-12-06 15:04:59 +0100

    Update gost-db-install.sh

--- gost-db-install.sh
+++ gost-db-install.sh
@@ -1,5 +1,7 @@
+# for Ubuntu 16.04, PostgreSQL 9.5
 sudo apt-get -y install postgresql postgresql-contrib postgis wget
 wget https://raw.githubusercontent.com/Geodan/gost-db/master/gost_init_db.sql
+/etc/init.d/postgresql start
 sudo su postgres -c psql << EOF
 ALTER USER postgres WITH PASSWORD 'postgres';
 CREATE DATABASE gost OWNER postgres;
