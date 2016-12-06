e5552a241667c6fab439d82c74825805e5584a6f
Bert Temme <btemme@hotmail.com>
2016-12-06 12:20:12 +0100

    Update gost-db-install.sh

--- gost-db-install.sh
+++ gost-db-install.sh
@@ -1,4 +1,6 @@
-sudo apt-get -y install postgresql postgresql-contrib postgis
+sudo apt-get -y install postgresql postgresql-contrib postgis wget
+wget https://github.com/Geodan/gost-db/blob/master/gost_init_db.sql
+sudo /etc/init.d/postgresql start
 sudo su postgres -c psql << EOF
 ALTER USER postgres WITH PASSWORD 'postgres';
 CREATE DATABASE gost OWNER postgres;
@@ -7,6 +9,4 @@ CREATE EXTENSION postgis;
 \q
 EOF
 
-# todo test : 
-wget https://github.com/Geodan/gost-db/blob/master/gost_init_db.sql
 # todo: run sql script
