16ddc22c171008a1bd5812ce04d93c514c757472
Bert Temme <btemme@hotmail.com>
2018-10-17 15:31:08 +0200

    Update gost-db-install.sh

--- gost-db-install.sh
+++ gost-db-install.sh
@@ -6,7 +6,6 @@ sudo su postgres -c psql << EOF
 ALTER USER postgres WITH PASSWORD 'postgres';
 CREATE DATABASE gost OWNER postgres;
 \connect gost
-CREATE EXTENSION postgis;
 \i gost_init_db.sql
 \q
 EOF
